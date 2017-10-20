543aa102d4f4693db0741c708b08b2cc9aed11bb
MuhammadAli0 <mohammedAli@technologist.com>
2017-10-20 23:03:31 +0200

    Add files via upload

---
+++ iptables/README.md
@@ -0,0 +1,4 @@
+# IpTables_5060
+
+run this line before start 
+sudo iptables -A INPUT -p tcp --destination-port 5060 -j DROP
---
+++ iptables/script.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+import os
+from sys import argv
+import  socket 
+
+
+
+file = "wite_list.ip"
+
+
+def Free_system():
+	os.system("iptables -F")
+	os.system("echo >  "+file+" ") 
+	return True
+
+def enable_ip(address):
+	mbr = 0
+	wite_list = open(file, 'r')
+	for ip in wite_list:
+		if (address in ip):
+			print "the %s alredy in wite list" % address
+			mbr = 1
+			break
+			
+	wite_list.close()
+	if (mbr == 0 ):
+		os.system("iptables -D INPUT -p tcp --destination-port 5060 -j DROP")
+		os.system("iptables -A INPUT -s"+ address +" -p tcp --destination-port 5060 -j ACCEPT") 
+		os.system("echo "+ address +" >> wite_list.ip")
+		os.system("iptables -A INPUT -p tcp --destination-port 5060 -j DROP")
+		return True
+
+
+
+def disable_ip(address):
+	ip = os.popen("more "+file+" |grep -o "+address+"", 'r').read()
+	if (address not in ip):
+		print "the %s  in not in wite list" % address
+	else:
+		os.system("iptables -D INPUT -s "+address+" -p tcp --destination-port 5060 -j ACCEPT")
+		ins = open(file).read()
+    		out = open(file, 'w')
+		
+		ins = ins.replace(address, "")
+		out.write(ins)
+		out.close()		
+		
+		#os.system("sed 's/%s/0/g' %s") % (address, file)
+
+def list_allowing_ip():
+
+	wite_list = open(file, 'r')
+	for line in wite_list:
+		print line
+	wite_list.close()
+
+
+def is_valid_ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  
+        return False
+
+    return True
+
+def main():
+	if (len(argv) == 1):
+		print"|*****************************************************************************|"
+		print"| use sudo ./script xxx.xxx.xxx.xxx  enable   [to enable ip address]          |"
+		print"| use sudo ./script XXX.XXX.XXX.XXX  disable  [to remove ip from wite list]   |"
+		print"| use sudo ./script list                      [to print list of wite list ip] |"
+		print"| use sudo ./script free                      [to free the system]            |"
+		print"|                                                                             |"
+		print"|*****************************************************************************|"
+	elif(argv[1].upper() == "HELP"):
+		print"|*****************************************************************************|"
+		print"| use sudo ./script xxx.xxx.xxx.xxx  enable   [to enable ip address]          |"
+		print"| use sudo ./script XXX.XXX.XXX.XXX  disable  [to remove ip from wite list]   |"
+		print"| use sudo ./script list                      [to print list of wite list ip] |"
+		print"| use sudo ./script free                      [to free the system]            |"
+		print"|                                                                             |"
+		print"|*****************************************************************************|"
+
+	elif(argv[1].upper()  == "LIST"):
+		list_allowing_ip()
+	elif(argv[1].upper() == "FREE"):
+		Free_system()
+	elif(is_valid_ipv4_address(argv[1])):
+		if(argv[2].upper() == "ENABLE"):
+			enable_ip(argv[1])
+		elif(argv[2].upper() == "DISABLE"):
+			disable_ip(argv[1])
+
+		else:
+			print "\n\t ERROR IN THE ACTION \n"
+			print "\t use (enable)   to block ip address \n"
+			print "\t use (disable) to unblock ip address\n"
+			print "\t use (list)    to list all blocking ip "
+	else:
+		print "/n/t plise enter valied ip adress /n/n"
+
+if __name__ == '__main__':
+	main()
+
